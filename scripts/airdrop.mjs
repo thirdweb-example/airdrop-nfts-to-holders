af06f49ae77d764f2efe3695510fd7b11376d850
Avneesh Agarwal <avneeshagarwal0612@gmail.com>
2022-10-24 09:36:54 +0530

    feat: init

---
+++ scripts/airdrop.mjs
@@ -0,0 +1,45 @@
+import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import fs from "fs";
+import path from "path";
+
+(async () => {
+  const sdk = new ThirdwebSDK("goerli");
+  const contract = await sdk.getContract(
+    "0x08d4CC2968cB82153Bb70229fDb40c78fDF825e8"
+  );
+
+  if (!contract) {
+    return console.log("Contract not found");
+  }
+
+  const nfts = await contract?.erc721.getAll();
+
+  if (!nfts) {
+    return console.log("No NFTs found");
+  }
+
+  const csv = nfts?.reduce((acc, nft) => {
+    const address = nft.owner;
+    const quantity = acc[address] ? acc[address] + 1 : 1;
+    return { ...acc, [address]: quantity };
+  }, {});
+
+  const filteredCsv = Object.keys(csv).reduce((acc, key) => {
+    if (key !== "0x0000000000000000000000000000000000000000") {
+      return {
+        ...acc,
+        [key]: csv[key],
+      };
+    }
+    return acc;
+  }, {});
+
+  const csvString =
+    "address,quantity\r" +
+    Object.entries(filteredCsv)
+      .map(([address, quantity]) => `${address},${quantity}`)
+      .join("\r");
+
+  fs.writeFileSync(path.join(path.dirname("."), "nfts.csv"), csvString);
+  console.log("Generated nfts.csv");
+})();
